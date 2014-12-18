f8f18e6eab69f3d663040aaa642d85e31ab43ef6
newlifechou <newlifechou@gmail.com>
2014-12-19 01:56:51 +0800

    smallchange

--- DailyHelper/MainWindow.xaml.cs
+++ DailyHelper/MainWindow.xaml.cs
@@ -35,10 +35,17 @@ namespace DailyHelper
             PlanDB plandb = new PlanDB();
             List<Plan> plans = plandb.GetPlans("where isfinished=0");
             lstPlan.ItemsSource = plans;
-
+            if (lstPlan.Items.Count > 0)
+            {
+                lstPlan.SelectedIndex = 0;
+            }
             FeeDB feedb=new FeeDB();
             List<Fee> fees = feedb.GetFeeList();
             lstFee.ItemsSource = fees;
+            if (lstFee.Items.Count>0)
+            {
+                lstFee.SelectedIndex = 0;
+            }
         }
         private void menuEditPlan_Click(object sender, RoutedEventArgs e)
         {
@@ -50,6 +57,11 @@ namespace DailyHelper
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            LoadData();
         }
     }
 }
