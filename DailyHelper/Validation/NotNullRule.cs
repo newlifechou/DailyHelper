29b7b4310e266bddb67aaa0db890e2795ee6ac9b
newlifechou <newlifechou@gmail.com>
2014-12-19 12:09:48 +0800

    create a validationrule

---
+++ DailyHelper/Validation/NotNullRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace DailyHelper
+{
+    /// <summary>
+    /// use the validate the null,empty value
+    /// </summary>
+    public class NotNullRule : ValidationRule
+    {
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (value == null || value.ToString() == "")
+            {
+                return new ValidationResult(false, "Should not be null or empty");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
